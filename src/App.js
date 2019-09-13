f9382628220ce1121c6c41f2211e2e5d6e901359
Dexter Dagorne <dj.dagorne@gmail.com>
2019-09-13 15:24:31 -0400

    try not to cry

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import './App.css';
 // import TheDate from './state/TheDate.js';
 // import Counter from './state/Counter/Counter.js';
 //import Tabs from './state/Tabs';
-import Accordion from './state-drills/Accordion.js'
+//import Accordion from './state-drills/Accordion.js'
+//import ShoppingList from './shopping-list/ShoppingList'
+import AddItemForm from './shopping-list/AddItemForm';
+import ShoppingList from './shopping-list/ShoppingList';
+
+class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+  handleAddItem = (itemName) => {
+    const newItems = [
+      ...this.state.shoppingItems,
+      { name: itemName, checked: false }
+    ]
+    this.setState({shoppingItems: newItems})
+  }
+  handleDeleteItem = (item) => { //should be an arrow function to avoid ES5 issues?
+    const newItems = this.state.shoppingItems.filter(itm => itm !== item)
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+  handleCheckItem = (item) => {
+    const newItems = this.state.shoppingItems.map(itm => {
+      if (itm === item){
+        item.checked = !itm.checked
+      }
+      return itm
+    })
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm 
+              onAddItem={this.handleAddItem} 
+            />
+          </section>
+          <section>
+            <ShoppingList items={this.state.shoppingItems}
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem} 
+            />
+          </section>
+        </main>
+      </>
+    )
+  }
+}
+
+export default App;
+
+//        {/* <Accordion /> */}
+//        {/* <Tabs tabs={tabsProp}/> */}
+//        {/* //<TheDate  /> */}
+//        {/* <RouletteGun bulletInChamber={8} /> */}
+//        {/* <Counter count={123} step={420}/> */}
 
 // const tabsProp = [
 //   { name: 'First tab',
@@ -13,21 +82,3 @@ import Accordion from './state-drills/Accordion.js'
 //   { name: 'Third tab',
 //     content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
 // ];
-
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Accordion />
-        {/* <Tabs tabs={tabsProp}/> */}
-        {/* //<TheDate  /> */}
-        {/* <RouletteGun bulletInChamber={8} /> */}
-        {/* <Counter count={123} step={420}/> */}
-      </div>
-    );
-  }
-}
-
-export default App;
